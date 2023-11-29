21c424058f3bd0988055ac66592c6a5b3682b542
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2023-11-28 17:19:52 -0800

    add private helper to detect durable function packages

--- src/Microsoft.Azure.AppConfiguration.Functions.Worker/AzureAppConfigurationRefreshMiddleware.cs
+++ src/Microsoft.Azure.AppConfiguration.Functions.Worker/AzureAppConfigurationRefreshMiddleware.cs
@@ -6,6 +6,7 @@ using Microsoft.Azure.Functions.Worker.Middleware;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,12 +20,14 @@ namespace Microsoft.Azure.AppConfiguration.Functions.Worker
         // The minimum refresh interval on the configuration provider is 1 second, so refreshing more often is unnecessary
         private static readonly long MinimumRefreshInterval = TimeSpan.FromSeconds(1).Ticks;
         private long _refreshReadyTime = DateTimeOffset.UtcNow.Ticks;
+        private readonly bool _isConfigureAwaitAllowed;
 
         private IEnumerable<IConfigurationRefresher> Refreshers { get; }
 
         public AzureAppConfigurationRefreshMiddleware(IConfigurationRefresherProvider refresherProvider)
         {
             Refreshers = refresherProvider.Refreshers;
+            _isConfigureAwaitAllowed = IsConfigureAwaitAllowed();
         }
 
         public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
@@ -48,7 +51,22 @@ namespace Microsoft.Azure.AppConfiguration.Functions.Worker
                 }
             }
 
-            await next(context).ConfigureAwait(false);
+            if (_isConfigureAwaitAllowed)
+            {
+                await next(context).ConfigureAwait(false);
+            }
+            else
+            {
+                #pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
+                await next(context);
+                #pragma warning restore CA2007
+            }
+        }
+
+        private static bool IsConfigureAwaitAllowed()
+        {
+            // Returns false if any durable function packages are loaded, true otherwise
+            return !AppDomain.CurrentDomain.GetAssemblies().Any(assembly => assembly.GetName().Name.StartsWith("Microsoft.DurableTask"));
         }
     }
 }
